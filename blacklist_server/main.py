d7d67d28f14c4850155c5f66f1dba0a38d31605c
Shadow_StarCloud <2970515877@qq.com>
2022-12-07 17:51:08 +0800

    添加云黑服务器系统，需要Flask模块

--- blacklist_server/main.py
+++ blacklist_server/main.py
@@ -1,10 +1,18 @@
 import sqlite3
 import traceback
+import time
 
+from sys import stderr
 from json import dumps
 from flask import Flask, request
 
 tokens = ["test"]
+log = open(fr'./logs/log-{time.strftime("%Y-%m-%d_%H-%M-%S")}.txt', 'w')
+
+
+def write_log(l: str):
+    log.write(f'''[{time.strftime("%x %X")}] {l}\n''')
+    log.flush()
 
 
 class sql:
@@ -22,12 +30,15 @@ class sql:
             self.conn.commit()
             self.cur.close()
             self.conn.close()
+            write_log(f"数据库处理结果:{sql_return_result}")
             return True, sql_return_result
         except sqlite3.OperationalError as e:
             traceback.print_exc()
+            write_log(f"数据库处理出错:{e}")
             return False, f"数据库处理时出错:{e}"
 
-    def detect(self, token, qq):
+    def detect(self, token):
+        write_log(f"请求云黑用户的信息...token={token}")
         if token in tokens:
             s, r = self.execute_sql(f"select * from black_list")
             if s:
@@ -35,20 +46,23 @@ class sql:
             else:
                 return False, r
         else:
+            write_log(f"token:'{token}'不在可用的tokens列表内")
             return False, "token不正确"
 
     def add(self, token, qq, group, reason):
+        write_log(f"添加云黑...token={token}, qq={qq}, group={group}, reason={reason}")
         if token in tokens:
-            print(qq)
             s, r = self.execute_sql(f"INSERT INTO black_list VALUES('{qq}','{group}','{reason}')")
             if s:
                 return True, "数据加入成功"
             else:
                 return False, r
         else:
+            write_log(f"token:'{token}'不在可用的tokens列表内")
             return False, "token不正确"
 
     def delete(self, token, qq):
+        write_log(f"删除云黑...token={token}, qq={qq}")
         if token in tokens:
             print(qq)
             s, r = self.execute_sql(f"delete from black_list where qq = '{qq}'")
@@ -57,6 +71,7 @@ class sql:
             else:
                 return False, r
         else:
+            write_log(f"token:'{token}'不在可用的tokens列表内")
             return False, "token不正确"
 
 
@@ -67,44 +82,55 @@ bl = Flask(__name__)
 @bl.route("/blacklist/", methods=["GET"])
 def blacklist():
     try:
-        s, r = bl_sql.detect(request.args.get("token"), request.args.get("qq"))
+        s, r = bl_sql.detect(request.args.get("token"))
         if s:
-            print(r)
+            print(f"Debug: 获取的数据:{r}")
+            write_log(f"获取的云黑用户元数据:{r}")
             return dumps({"data": r}), 200
         else:
             return dumps({"msg": r}), 403
     except Exception as e:
+        traceback.print_exc(file=log)
         traceback.print_exc()
+        stderr.write("看起来出错了呢...要不提交一下issue?")
         return dumps({"msg": e}), 403
 
 
 @bl.route("/blacklist/add/", methods=["GET"])
 def add_blacklist():
     try:
+        print("接受到添加云黑请求：QQ:{0}, 群组:{1}，理由:{2}".format(request.args.get("QQ"), request.args.get("groupID"), request.args.get("reason")))
         s, r = bl_sql.add(request.args.get("token"), request.args.get("QQ"), request.args.get("groupID"), request.args.get("reason"))
         if s:
             return dumps({"data": r}), 200
         else:
             return dumps({"msg": r}), 403
     except Exception as e:
+        traceback.print_exc(file=log)
         traceback.print_exc()
+        stderr.write("看起来出错了呢...要不提交一下issue?")
         return dumps({"msg": e}), 403
 
 
 @bl.route("/blacklist/delete/", methods=["GET"])
 def del_blacklist():
     try:
+        print("接受到删除云黑请求：QQ:{0}".format(request.args.get("QQ")))
         s, r = bl_sql.delete(request.args.get("token"), request.args.get("QQ"))
         if s:
             return dumps({"data": r}), 200
         else:
             return dumps({"msg": r}), 403
     except Exception as e:
+        traceback.print_exc(file=log)
         traceback.print_exc()
+        stderr.write("看起来出错了呢...要不提交一下issue?")
         return dumps({"msg": e}), 403
 
 
 if __name__ == "__main__":
+    print("启动项目...")
+    write_log("云黑服务器启动...")
     bl.run(host="0.0.0.0", port=5432)
 else:
     raise ImportError("此文件不可被引入")
